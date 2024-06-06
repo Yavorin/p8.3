9df670de2319d47aa6159a4d67d325e4462a35f7
Yavorin <159899546+Yavorin@users.noreply.github.com>
2024-06-06 13:41:48 +0300

    Add files via upload

---
+++ file.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $c = $_POST['c'];
+        $d = $_POST['d'];
+    // Перевіряємо, чи введені значення є цілими числами
+    if (is_numeric($c) && is_numeric($d)) {
+        // Виконуємо математичні операції
+        $sum = $c + $d;
+        $product = $c * $d;
+        $difference = $c - $d;
+        $quotient = $c / $d;
+        // Виводимо результати
+        echo "Сума: " . $sum . "<br>";
+        echo "Добуток: " . $product . "<br>";
+        echo "Різниця: " . $difference . "<br>";
+        echo "Частка: " . $quotient . "<br>";
+    }
+}
+?> 
